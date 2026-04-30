f366dbb352c1200b7acff3863153185b651f9ab8
jake <A00027435@mytudublin.ie>
2026-04-30 19:18:47 +0100

    fixed

---
+++ campus-companion/app/recommendations/page.tsx
@@ -0,0 +1,10 @@
+import RecommendedEvents from "../components/RecommendedEvents_example";
+
+export default function RecommendationsPage() {
+  return (
+    <main className="container">
+      <h1>Recommended Events</h1>
+      <RecommendedEvents />
+    </main>
+  );
+}
